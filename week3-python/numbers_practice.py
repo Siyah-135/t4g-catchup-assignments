252a8b1a3a42dd04ea7f243f706d7a629e22b87c
Siyah-135 <hasiyatuismail@gmail.com>
2026-03-05 23:10:14 +0000

    Add numbers practice exercises

---
+++ week3-python/numbers_practice.py
@@ -0,0 +1,48 @@
+#Task 1
+x =10
+y =8
+
+num1 = 2.52
+num2 =3.64
+
+
+print(x + y) #This is the addition operations that sums up the variables
+
+print(x - y) #This is the subtraction operations that subtracts the variables
+
+print(x * y) #This is the multiplication operations that multiplies the variables
+
+print(x / y) #This is the division operations that divides the variables and always returns a float value
+
+print(x // y) #This is the floor division operations that divides the variables but excludes decimals
+
+print(x % y) #This is the modulo operations that returns the remainder
+
+print(x ** y) #This is the exponentiation operations that raises x to the power of y
+
+
+#Task 2
+length = 7
+width = 5
+
+area = length * width
+perimeter = 2 * (length + width)
+
+print(f"A rectangle with length {length} and width {width} has an area of {area} and a perimeter of {perimeter}")
+
+
+#Task 3
+age =15
+age = float(age)
+
+height = 5.76
+height =int(height)
+
+print(float(age))
+print(int(height))
+
+
+print(type(age))
+print(type(height))
+print(type(area))
+print(type(num1))
