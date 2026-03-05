94b54aa294c753bc25a7f79f975b44fb453ad419
Siyah-135 <hasiyatuismail@gmail.com>
2026-03-05 23:10:58 +0000

    Add profile card file

---
+++ week3-python/profile_card.py
@@ -0,0 +1,25 @@
+   
+name = 'Hasiyatu Ismail'
+Cohort = 4
+group = "Backend_class"
+age = 21
+height = 5.67
+
+print("-----------------------------")
+print("      Profile Card     ")
+print("-----------------------------")
+print(f"Name: {name}")
+print(f"Cohort: {Cohort}")
+print(f"Group: {group}")
+print(f"Age: {age}")
+print(f"Height: {height}")
+print("-----------------------------")
+
+#footer
+
+current_week = 3
+
+weeks_left = 12 - current_week
+
+
+print(f"Weeks left: {weeks_left}")
